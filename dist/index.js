36e4966468951c3b73245b7b1f4944783d550710
nvillalon1994 <70975528+nvillalon1994@users.noreply.github.com>
2022-08-30 11:01:17 -0300

    first commit

---
+++ dist/index.js
@@ -0,0 +1,26 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+// Configuration the .env file
+dotenv_1.default.config();
+// Create Express APP
+const app = (0, express_1.default)();
+const port = process.env.PORT || 8000;
+// Define the first route of APP
+app.get('/', (req, res) => {
+    // Send HELLO WORD
+    res.send('welcome to APP Express +Nodemon + Jest + TS + Swagger + Mongoose');
+});
+app.get('/hello', (req, res) => {
+    // Send HELLO WORD
+    res.send('Buenas');
+});
+//execute APP and listen Reques to Port
+app.listen(port, () => {
+    console.log(`EXPRESS SERVER: Running at http://localhost:${port}`);
+});
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,30 @@
+import express,{Express,Request,Response} from 'express'
+import dotenv from 'dotenv'
+
+
+// Configuration the .env file
+dotenv.config();
+
+// Create Express APP
+
+const app: Express = express();
+const port = process.env.PORT || 8000;
+
+// Define the first route of APP
+
+app.get('/',(req:Request, res:Response) => {
+    // Send HELLO WORD
+    res.send('welcome to APP Express +Nodemon + Jest + TS + Swagger + Mongoose')
+})
+app.get('/hello',(req:Request, res:Response) => {
+    // Send HELLO WORD
+    res.send('Buenas')
+})
+
+
+
+//execute APP and listen Reques to Port
+app.listen(port, ()=>{
+    console.log(`EXPRESS SERVER: Running at http://localhost:${port}`)
+})
+
